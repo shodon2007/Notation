834f185e468eb4d0ec3d8f80773b0a3e3178695c
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-10-28 17:31:24 +0700

    functional version

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-let buttons = document.querySelectorAll(".button");
 let answer = document.getElementById("answer");
+let input = document.getElementById("input");
 let plans = [2, 8, 10, 16, 32];
 
 let whichButtonTo = 10;
@@ -10,16 +10,18 @@ let whichButtonFrom = 2;
 
 const buttonClick = (value, type) => {
     removeClass(type);
-    const types = document.getElementById(type + value);
+    addClass(type, value);
 
-    if (type == "t") {
+    if (type == "to") {
         whichButtonTo = value;
     } else {
         whichButtonFrom = value;
     }
 
-    types.classList.add("checked");
+
+    output();
 }
+
 
 
 
@@ -30,4 +32,14 @@ const removeClass = (type) => {
     for (let number of plans) {
         document.getElementById(type + number).classList.remove("checked");
     }
+}
+
+const addClass = (type, value) => {
+    const types = document.getElementById(type + value);
+    types.classList.add("checked");
+}
+
+const output = () => {
+    input = document.getElementById("input");
+    console.log(input.value);
 }
