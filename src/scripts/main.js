3e13b91b1a5b0e2d3380b8e55c5ce02caca305c9
Speed431 <73215393+Speed431@users.noreply.github.com>
2023-01-13 10:02:29 +0700

    added webpack

---
+++ src/scripts/main.js
@@ -0,0 +1,45 @@
+let answer = document.getElementById("answer");
+let input = document.getElementById("input");
+let plans = [2, 8, 10, 16, 36];
+let outvalue;
+
+let whichButtonTo = 10;
+let whichButtonFrom = 2;
+
+
+
+
+window.buttonClick = (value, type) => {
+    removeClass(type);
+    addClass(type, value);
+
+    if (type == "to") {
+        whichButtonTo = value;
+    } else {
+        whichButtonFrom = value;
+    }
+
+    output();
+}
+
+
+
+window.output = () => {
+    outvalue = parseInt(input.value, whichButtonTo);
+    outvalue = outvalue.toString(whichButtonFrom);
+
+    answer.innerHTML = outvalue;
+}
+
+
+
+window.removeClass = (type) => {
+    plans.forEach((item) => {
+        document.getElementById(type + item).classList.remove("checked");
+    })
+}
+
+window.addClass = (type, value) => {
+    const types = document.getElementById(type + value);
+    types.classList.add("checked");
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+const isDev = process.env.NODE_ENV == 'development';
+const isProd = !isDev;
+
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        port: '2007',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd,
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize: isProd,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
+}
